318939c91a9413501f67fd869c4fcd301185b945
Magdalena <magda.g2112@gmail.com>
2022-12-09 16:20:28 +0100

    add christmas tree code

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function printChristmasTree(height) {
+	let rowShift = height - 1;
+	let rowLength = 1;
+
+	while (rowShift >= 0) {
+		printChristmasTreeRow(rowLength, rowShift);
+		rowLength += 2;
+		rowShift -= 1;
+	}
+}
+
+function printChristmasTreeRow(rowLength, rowShift) {
+	const spaces = ' '.repeat(rowShift);
+	const stars = '*'.repeat(rowLength);
+
+	console.log(spaces + stars);
+}
+
+printChristmasTree(9);
